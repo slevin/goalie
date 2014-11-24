a53fe22ba9198fa51fc7eaab4d37b971d7ec5caf
Sean Levin <sean.levin@gmail.com>
2014-11-24 12:19:34 -0500

    some mode setup

--- goalie.el
+++ goalie.el
@@ -17,17 +17,44 @@
 
 ;;; Code:
 
-;; code goes here
+(defvar goalie-mode-map
+  (let ((map (make-keymap)))
+    (suppress-keymap map t)
+    (define-key map (kbd "n") 'goalie-goto-next)
+    (define-key map (kbd "p") 'goalie-goto-previous)
+    (define-key map (kbd "RET") 'goalie-execute)
+    map)
+  "Goalie key map.")
+
+
+(define-derived-mode goalie-mode special-mode "Goalie"
+  "Major mode for Goalie."
+  )
+
+(defun goalie-goto-next ()
+  (interactive)
+  (message "goto next"))
+
+(defun goalie-goto-previous ()
+  (interactive)
+  (message "goto previous"))
+
+(defun goalie-execute ()
+  (interactive)
+  (message "execute"))
+
 
 (defun goalie-initialize-ui ()
   (switch-to-buffer "*goalie*")
-  (setq buffer-read-only t))
+  (goalie-mode))
 
 (defun goalie-render-ui ()
   (let ((inhibit-read-only t))
-    (progn  (insert " a buffer\nsomething")
-            (insert "\n")
-            (insert "another line"))))
+    (erase-buffer)
+    (insert "Yesterday's Commitments (Date goes here)\n")
+    (insert "some lines about commitments\n\n")
+    (insert "Today's Commitments (Date goes here)\n")
+    (insert "some lines about todays commitments\n")))
 
 ;; could have rerender whole thing based on updates
 ;; first is given a buffer var draw in
@@ -35,7 +62,7 @@
   "Start goalie."
   (interactive)
   (goalie-initialize-ui)
-  (render-ui)
+  (goalie-render-ui)
   ;; start up goalie "object"
   ;; creates/opens buffer
 
